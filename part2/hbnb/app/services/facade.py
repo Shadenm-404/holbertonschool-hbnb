b049e9b57c5204850573ba3cee8e3d5365856dd8
Shaden Alalwani <viuxwl7@gmail.com>
2026-01-07 12:29:27 +0300

    Update facade.py

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,6 +1,6 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
-from app.models.amenity import amenity
+from app.models.amenity import Amenity
 
 
 class HBnBFacade:
