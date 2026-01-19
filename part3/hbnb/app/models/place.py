5f23bcfaeafe951c58d144d0fe5dd3cbba958f6d
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2026-01-19 03:50:17 +0300

    Refactor Place model to use SQLAlchemy ORM

--- part3/hbnb/app/models/place.py
+++ part3/hbnb/app/models/place.py
@@ -1,22 +1,24 @@
-from app.models.base_model import BaseModel, Base
-from app.models.validation import Validator
+from sqlalchemy import Column, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from app.models.base_model import Base, BaseModel
 
-class Place(BaseModel):
-    def __init__(self, title, owner_id, description="", price_per_night=0):
-        super().__init__()
-
-        self.title = Validator.validate_string(title, "Title")
-        self.owner_id = owner_id
-        self.description = Validator.validate_string(description, "Description", required=False)
-        self.price_per_night = price_per_night
-
+class Place(Base, BaseModel):
+    __tablename__ = "places"
+    title = Column(String(100), nullable = False)
+    owner_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    description = Column(String(1024), default ="")
+    price_per_night = Column(Float, default=0.0)
+    
+    owner = relationship("User" , back_populates="places")
+    reviews = relationship("Review", back_populates="place", cascade="all, delete-orphan")
+    
     def to_dict(self):
-        data = super().to_dict()
-        data.update({
-            "title": self.title,
-            "owner_id": self.owner_id,
-            "description": self.description,
-            "price_per_night": self.price_per_night
-        })
-        return data
-
+        return {
+             "id": self.id,
+             "title": self.title,
+             "owner_id": self.owner_id,
+             "description": self.description,
+             "price_per_night": self.price_per_night,
+             "created_at": self.created_at.isoformat() if self.created_at else None,
+             "updated_at": self.updated_at.isoformat() if self.updated_at else None,
+    }
