9dd68c0577c1f439f7ae6298a12aead6b666cb25
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2026-01-19 14:20:46 +0300

    Disable amenities namespace in app initialization
    Comment out amenities namespace registration.

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -4,7 +4,7 @@ from flask_jwt_extended import JWTManager
 
 from app.api.v1.users import api as users_ns
 from app.api.v1.auth import api as auth_ns
-from app.api.v1.amenities import api as amenities_ns
+#from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 
 jwt = JWTManager()
@@ -30,7 +30,7 @@ def create_app(config_class):
     # Register namespaces
     api.add_namespace(users_ns, path="/api/v1/users")
     api.add_namespace(auth_ns, path="/api/v1/auth")
-    api.add_namespace(amenities_ns, path="/api/v1/amenities")
+    #api.add_namespace(amenities_ns, path="/api/v1/amenities")
     api.add_namespace(places_ns, path="/api/v1/places")
 
     return app
