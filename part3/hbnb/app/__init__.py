93a5f67b197895bc5c7229424fbfbf37a426665f
Shaden Alalwani <viuxwl7@gmail.com>
2026-01-21 12:10:25 +0300

    Update __init__.py

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -31,7 +31,7 @@ def create_app(config_class):
     # Register namespaces
     api.add_namespace(users_ns, path="/api/v1/users")
     api.add_namespace(auth_ns, path="/api/v1/auth")
-    #api.add_namespace(amenities_ns, path="/api/v1/amenities")
+    api.add_namespace(amenities_ns, path="/api/v1/amenities")
     api.add_namespace(places_ns, path="/api/v1/places")
     api.add_namespace(reviews_ns, path="/api/v1/reviews")
 
