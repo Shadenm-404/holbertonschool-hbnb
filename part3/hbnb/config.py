9058e947d26d06857e5fedb45856d07c5da47891
Nada Al-Mutairi <nada.ghannam19@gmail.com>
2026-01-27 16:23:38 +0300

    Update config.py to use hardcoded secret keys

--- part3/hbnb/config.py
+++ part3/hbnb/config.py
@@ -1,24 +1,13 @@
-import os
-
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt_secret_key")
-
+    SECRET_KEY = "super-secret-key"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = False
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///hbnb_dev.db"
-
-
-class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///development.db"
 
 
 config = {
     "development": DevelopmentConfig,
-    "production": ProductionConfig,
-    "default": DevelopmentConfig
 }
