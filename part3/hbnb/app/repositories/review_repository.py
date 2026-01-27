0fb96c422901c32e09484fdd3452d65fc885cfe0
Shaden Alalwani <viuxwl7@gmail.com>
2026-01-27 16:41:20 +0300

    Add ReviewRepository for managing reviews

--- part3/hbnb/app/repositories/review_repository.py
+++ part3/hbnb/app/repositories/review_repository.py
@@ -1,1 +1,10 @@
 from app.repositories.sqlalchemy_repository import SQLAlchemyRepository
+from app.models.review import Review
+
+
+class ReviewRepository(SQLAlchemyRepository):
+    def init(self):
+        super().init(Review)
+
+    def get_by_user_and_place(self, user_id, place_id):
+        return Review.query.filter_by(user_id=user_id, place_id=place_id).first()
