3b3f2b1c7cd42396bdf6b3a88739c76137235899
Shaden Alalwani <viuxwl7@gmail.com>
2026-02-14 22:14:48 +0300

    Update place_repository.py

--- part3/hbnb/app/repositories/place_repository.py
+++ part3/hbnb/app/repositories/place_repository.py
@@ -4,8 +4,8 @@ from app.models.place import Place
 
 
 class PlaceRepository(SQLAlchemyRepository):
-    def __init__(self):
-        super().__init__(Place)
+    def init(self):
+        super().init(Place)
 
     def get_by_owner(self, user_id):
         return self.model.query.filter_by(owner_id=user_id).all()
